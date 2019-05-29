5bcdd31bd240c74bf0e6fc9c434b1f526b887c0c
Serguei Cambour <serguei.cambour.partner@decathlon.com>
2019-05-29 16:39:24 +0200

    Implement alert function

--- lib/gallows_web/views/hangman_view.ex
+++ lib/gallows_web/views/hangman_view.ex
@@ -1,3 +1,26 @@
 defmodule GallowsWeb.HangmanView do
   use GallowsWeb, :view
+
+  @responses = %{
+    :won          => { :success, "You won!"},
+    :lost         => { :danger, "You Lost!"},
+    :good_guess   => { :success, "Good guess!"},
+    :bad_guess    => { :waning, "Bad guess!"},
+    :already_used => { :info, "You already guessed that"}
+  }
+  def game_state(state) do
+    @responses[state]
+    |> alert()
+  end
+
+  defp alert(nil), do ""
+
+  defp alert({class, message}) do
+    """
+      <div class="alert alert-#{class}">
+        #{message}
+      </div>
+    """
+    |> raw()
+  end
 end
